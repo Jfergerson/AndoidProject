6f654f6f9ccc2b2602dea638e0825f6001f2ecd2
zyshara <hi.imma.bear@gmail.com>
2017-05-18 04:12:56 -0700

    Updated stuff

--- MyApplication/app/src/main/java/com/example/john/johnjenproject/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/john/johnjenproject/MapsActivity.java
@@ -11,6 +11,7 @@ import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
@@ -38,54 +39,96 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Iterator;
 import java.util.Random;
 
+import static android.os.SystemClock.sleep;
 import static com.example.john.johnjenproject.R.id.map;
+import static com.example.john.johnjenproject.R.id.name;
+import static com.example.john.johnjenproject.R.id.ratingBar;
 
 public class MapsActivity extends FragmentActivity implements
         OnMapReadyCallback,
         GoogleMap.OnMyLocationButtonClickListener,
         ActivityCompat.OnRequestPermissionsResultCallback,
-        LocationListener{
+        LocationListener
+{
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private static int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
     private GoogleMap mMap;
     private boolean mPermissionDenied = false;
     private LocationManager locationManager;
     private AlertDialog.Builder builder;
+
     private static final long MIN_TIME = 400;
     private static final float MIN_DISTANCE = 1000;
-    public static String review;
-    public static float rating;
 
+    private static JSONObject curTruck;
+
+    private static JSONObject mTacoTrucks;
+    private static GetTacoTrucksTask mGetTacoTrucksTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(map);
         mapFragment.getMapAsync(this);
         SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.shared_pref_key),MODE_PRIVATE);
-        String s = sharedPreferences.getString("result",null);
+        String s = sharedPreferences.getString("result", null);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-     //  locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this); //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER
 
-        if(s != null){//do some stuff to parse vals out of s but do the parsing in on map ready
-            //so we can just load in those pointer right away.
+        mGetTacoTrucksTask = new MapsActivity.GetTacoTrucksTask();
+        mGetTacoTrucksTask.execute((Void) null);
 
+        if (s != null){ }
+    }
+
+    public class GetTacoTrucksTask extends AsyncTask<Void, Void, Boolean> {
+
+        GetTacoTrucksTask() {
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                URL url = new URL("http://104.236.190.64:8000/tacotruck/");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                int i = httpURLConnection.getResponseCode();
+                Log.d("Got response ", Integer.toString(i));
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line+"\n");
+                }
+                br.close();
+                Log.d("JSON ", sb.toString());
+                mTacoTrucks = new JSONObject(sb.toString());
+            }
+            catch (Exception e) {
+                Log.d("Exception: ", e.toString());
+            }
+            return true;
         }
     }
+
     @Override
     public void onLocationChanged(Location location) {
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
-       // map
         locationManager.removeUpdates(this);
     }
 
@@ -98,76 +141,35 @@ public class MapsActivity extends FragmentActivity implements
     @Override
     public void onProviderDisabled(String provider) { }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setOnInfoWindowClickListener(getInfoWindowClickListener());
         enableMyLocation();
-        // Add a marker in Sydney and move the camera
-        Location location;
-        LatLng cur_loc = null;
+
+        while(mTacoTrucks == null) { sleep(500); }
+
         try {
-            if (mMap.isMyLocationEnabled()) {
-                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                if(location != null) {
-                    cur_loc = new LatLng(location.getLatitude(), location.getLongitude());
-                }
+            JSONArray results = mTacoTrucks.getJSONArray("results");
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject t = results.getJSONObject(i);
+                Log.d("Got truck ", t.toString());
+
+                LatLng truck = new LatLng(t.getDouble("lat"), t.getDouble("lon"));
+                Marker marker = mMap.addMarker(new MarkerOptions().position(truck).title(t.getString("name")));
+                marker.setTag(t);
             }
-        }catch(SecurityException e){Log.d("sec except",e.toString());}
-
-        LatLng truck_A = new LatLng(39.724315, -121.849576);
-        LatLng truck_B = new LatLng(39.730541, -121.858539);
-        LatLng truck_C = new LatLng(39.725461, -121.834747);
-        float[] dbl = new float[3];
-        Marker marker = mMap.addMarker(new MarkerOptions().position(truck_A).title("Truck A"));
-        if(cur_loc!= null){
-            Location.distanceBetween(truck_A.latitude,truck_A.longitude,cur_loc.latitude,cur_loc.longitude,dbl);
-            marker.setTag(dbl[0]);
         }
-        marker = mMap.addMarker(new MarkerOptions().position(truck_C).title("Truck C"));
-        if(cur_loc!= null){
-            Location.distanceBetween(truck_B.latitude,truck_B.longitude,cur_loc.latitude,cur_loc.longitude,dbl);
-            marker.setTag(dbl[0]);
+        catch (Exception e) {
+            Log.d("Exception: ", e.toString());
         }
-        marker = mMap.addMarker(new MarkerOptions().position(truck_B).title("Truck B"));
-        if(cur_loc!= null){
-            Location.distanceBetween(truck_C.latitude,truck_C.longitude,cur_loc.latitude,cur_loc.longitude,dbl);
-            marker.setTag(dbl[0]);
-        }
-
-
-        Log.d("returned from distbet",Float.toString(dbl[0]));
-        Log.d("returned from dbl1",Float.toString(dbl[1]));
-        Log.d("returned from dbl2",Float.toString(dbl[2]));
-
-
-
-
-
-
-
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(chico_A));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(chico_A));
 
     }
     private void enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission to access the location is missing.
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
-        } else if (mMap != null) {
-            // Access to the location has been granted to the app.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+        else if (mMap != null) {
             mMap.setMyLocationEnabled(true);
         }
     }
@@ -208,85 +210,84 @@ public class MapsActivity extends FragmentActivity implements
 
         return false;
     }
+
     public GoogleMap.OnInfoWindowClickListener getInfoWindowClickListener(){
         return new GoogleMap.OnInfoWindowClickListener() {
             @Override
             public void onInfoWindowClick(Marker marker) {
-                if(marker.getTag() != null) {
-                    Toast.makeText(getApplicationContext(), "You are: " + (float) marker.getTag() / 1000 + " KM from this truck", Toast.LENGTH_LONG).show();
+                try {
+                    JSONObject tag = (JSONObject) marker.getTag();
+
+                    enableMyLocation();
+                    Location location;
+                    LatLng cur_loc = null;
+                    if (mMap.isMyLocationEnabled()) {
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if(location != null) {
+                            cur_loc = new LatLng(location.getLatitude(), location.getLongitude());
+                        }
+                    }
+
+                    float[] f = new float[5];
+                    Location.distanceBetween(tag.getDouble("lat"), tag.getDouble("lon"), cur_loc.latitude, cur_loc.longitude, f);
+                    Toast.makeText(getApplicationContext(), "You are: " + f[0] / 1000 + " KM from this truck", Toast.LENGTH_LONG).show();
+
+                    DialogFragment fire = new CustomDialogFragment();
+                    fire.show(getSupportFragmentManager(), "missile");
+
+                    curTruck = tag;
                 }
-                DialogFragment fire = new CustomDialogFragment();
-
-                //this is where you are setting up all the review text and
-                //rating score that is displayed in the little popup menu
-                //add any more images you want to use to the drawables folder manually
-                //there is not a way to add them through android studio so drag and drop images
-                //into the /res/drawable/ folder and use them that way
- 
-
-                review = "hello there";
-                Random r = new Random();
-                rating = (r.nextInt(50)/10f);
-//              review = jsonArray.getjson(marker.getTag()).getString(marker.title())
-                //rating = jsonArray.getjson(marker.getTag()).getString(marker.title()).
-
-
-                fire.show(getSupportFragmentManager(),"missile");
-
+                catch(SecurityException e){
+                    Log.d("Security Exception", e.toString());
+                }
+                catch (Exception e) {
+                    Log.d("Exception", e.toString());
+                }
             }
         };
     }
     public static class CustomDialogFragment extends DialogFragment {
-        /** The system calls this to get the DialogFragment's layout, regardless
-         of whether it's being displayed as a dialog or an embedded fragment. */
+
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            // Inflate the layout to use as dialog or embedded fragment
-
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             return inflater.inflate(R.layout.display_truck, container, false);
         }
 
-        /** The system calls this only when creating the layout in a dialog. */
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // The only reason you might override this method when using onCreateView() is
-            // to modify any dialog characteristics. For example, the dialog includes a
-            // title by default, but your custom layout might not need it. So here you can
-            // remove the dialog title, but you must call the superclass to get the Dialog.
+
             Dialog dialog = super.onCreateDialog(savedInstanceState);
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialogInterface, int id){
                 }
             });
+
             builder.setView(R.layout.display_truck);
             LayoutInflater inflater = getActivity().getLayoutInflater();
             ViewGroup vg = (ViewGroup)inflater.inflate(R.layout.display_truck, null);
+
             ImageView image = (ImageView) vg.findViewById(R.id.truckimg);
             TextView text = (TextView) vg.findViewById(R.id.textview);
             RatingBar ratingBar = (RatingBar) vg.findViewById(R.id.ratingBar);
+            TextView name = (TextView) vg.findViewById(R.id.name);
 
-            //set text and stuff above here where i set the reveiw and rating to
-            //random shit just parse out the relevent bits from the json and set them there
-            text.setText(review);
-            ratingBar.setRating(rating);
+            try {
+                name.setText(curTruck.getString("name"));
+                text.setText(curTruck.getString("hours"));
+                ratingBar.setRating(curTruck.getInt("rating"));
 
-            //this is where you can set the rating that you pass from the json
-            //into the program
-            if(rating > 2){
-                image.setImageResource(R.drawable.foodtruck);
-
-            } else {
-                image.setImageResource(R.drawable.coffeespider);
+                if (curTruck.getInt("rating") > 2) {
+                    image.setImageResource(R.drawable.foodtruck);
+                } else {
+                    image.setImageResource(R.drawable.coffeespider);
+                }
+            }
+            catch (Exception e) {
+                Log.d("Exception", e.toString());
             }
 
-
-
             builder.setView(vg);
-
-
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
             return builder.create();
         }
